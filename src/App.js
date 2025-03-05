d83841980476794258853a4141abec6ba32e3560
Kausheya2006 <royrimo2006@gmail.com>
2025-03-05 23:59:58 +0530

    sample commit message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello from React!</h1>
-      <p>This is a simple React frontend running inside a Docker container.</p>
+      <p>This is a simple React0 frontend running inside a Docker container.</p>
     </div>
   );
 }
